f7d16579c68558fb10fe14346ddd5455bd6f1f80
Wanderley Flores <wanderley.flores@udla.edu.ec>
2023-10-18 08:35:23 -0500

    inicio taller 2 Abrir proyecto mediante el link con clonacion

---
+++ Migrations/20231018125153_Inicio.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WanderleyFlores_Ejercicio1CF.Migrations
+{
+    public partial class Inicio : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ Models/Promo.cs
@@ -0,0 +1,11 @@
+﻿namespace WanderleyFlores_Ejercicio1CF.Models
+{
+    public class Promo
+    {
+        public int PromoId { get; set; }
+        public string? PromoName { get; set; }
+        public string? PromoDescription { get; }
+        public int Id {  get; set; }
+        public Burger? Burger { get; set; }
+    }
+}
